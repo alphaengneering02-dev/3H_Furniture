ed99d11b8c690be3347e36145be974999cf8bafb
taeyang999-kim <sunsun9579@naver.com>
2026-04-28 09:07:01 +0900

    0428 김태양
    admins, delivery 엔티티와 DTO 만들었습니다.

---
+++ threeh/src/main/java/com/cmyk/threeh/dto/AdminsDTO.java
@@ -0,0 +1,15 @@
+package com.cmyk.threeh.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminsDTO {
+
+    private Long adminId;
+    private String adLoginId;
+    private String password;
+    private String adminName;
+    
+}
