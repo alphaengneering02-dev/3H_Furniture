4724f1fc8587bdc92c492c709d365991fd87b8ec
soeun <yuthdms0929@gmail.com>
2026-04-28 12:37:09 +0900

    [fix]Member, Bookmarks 수정
    Member, Bookmarks.java 수정
    MemberTests(회원가입, 회원정보 조회 테스트) 추가

--- threeh/src/main/java/com/cmyk/threeh/domain/Bookmarks.java
+++ threeh/src/main/java/com/cmyk/threeh/domain/Bookmarks.java
@@ -2,14 +2,14 @@ package com.cmyk.threeh.domain;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +21,7 @@ public class Bookmarks {
 
     @Id  //Primary Key
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookmakr_Id")
     private Long bookmakrId;
 
     //member_id (외래키)
@@ -29,12 +30,14 @@ public class Bookmarks {
     private Member member;
 
     //item_id (외래키)
-    // @OneToOne
-    // @JoinColumn(name = "itemId")
-    // private Item item;
+    @OneToOne
+    @JoinColumn(name = "itemId")
+    private Item item;
 
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @Column(name = "type")
     private String type;
     
 }
