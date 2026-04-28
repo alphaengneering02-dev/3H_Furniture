fc1aca04147f95343c5551c26421bd723983e975
alphaengneering02-dev <alphaengneering02@gmail.com>
2026-04-28 09:23:42 +0900

    Revert "0428 김태양"
    This reverts commit ed99d11b8c690be3347e36145be974999cf8bafb.

--- threeh/src/main/java/com/cmyk/threeh/domain/Delivery.java
+++ threeh/src/main/java/com/cmyk/threeh/domain/Delivery.java
@@ -1,45 +1,5 @@
 package com.cmyk.threeh.domain;
 
-import javax.persistence.*;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Table(name = "DELIVERY")
-@Getter
-@Setter
 public class Delivery {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "DELIVERY_ID")
-    private Long deliveryId;
-
-    @Column(name = "ADMIN_ID", nullable = false)
-    private Long adminId;
-
-    @Column(name = "COMPANY_NAME", nullable = false)
-    private String companyName;
-
-    @Column(name = "BUSINESS_NAME", nullable = false)
-    private String businessName;
-
-    @Column(name = "BUSINESS_PHONE", nullable = false)
-    private String businessPhone;
-
-    @Column(name = "BUSINESS_NO", nullable = false)
-    private String businessNo;
-
-    @Column(name = "BUSINESS_ADDR", nullable = false)
-    private String businessAddr;
-
-    @Column(name = "DELIVERY_NAME", nullable = false)
-    private String deliveryName;
-
-    @Column(name = "DELIVERY_PHONE", nullable = false)
-    private String deliveryPhone;
-
-    @Column(name = "DELIVERY_CARNO", nullable = false)
-    private String deliveryCarNo;
+    
 }
