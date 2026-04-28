deb2c4c6f429cb7a46fb0854adc5aeb0f5a6701e
dlsghsla4623-cloud <dlsghsla4623@gmail.com>
2026-04-28 17:20:02 +0900

    [feat] 문제있는 클래스 추가수정
    추가수정

--- threeh/src/main/java/com/cmyk/threeh/domain/Cart.java
+++ threeh/src/main/java/com/cmyk/threeh/domain/Cart.java
@@ -1,41 +1,22 @@
 package com.cmyk.threeh.domain;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-
-
-
+import javax.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "cart")
-
-@Getter
+@Getter 
 @Setter
 @NoArgsConstructor
 public class Cart {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cart_seq")
-    @SequenceGenerator(name = "cart_seq", sequenceName = "cert_SEQ", allocationSize = 1)
+    @SequenceGenerator(name = "cart_seq", sequenceName = "CART_SEQ", allocationSize = 1)
     @Column(name = "cart_id")
     private Long cartId;
 
@@ -43,7 +24,7 @@ public class Cart {
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItem> cartItems = new ArrayList<>();
 
 }
