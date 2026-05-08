b81331d2ebccb789b5f790be5efa54933a1ae974
taeyang999-kim <sunsun9579@naver.com>
2026-05-08 14:03:34 +0900

    save local changes

--- threeh/src/main/frontend/src/App.js
+++ threeh/src/main/frontend/src/App.js
@@ -5,8 +5,12 @@ import Signup from './components/Signup';
 import Signup_site from './components/Signup_site';
 import ChangePw from './components/ChangePW';
 import Order from './components/Order';
-import Main from './components/Main';
+
 import Login from './components/Login';
+import AddDelivery from './components/AddDelivery';
+import AdminDashboard from './components/AdminDashboard';
+import AddProduct from './components/AddProduct';
+import AdminDashboardCopy from './components/AdminDashboardCopy';
 
 
 
@@ -14,18 +18,21 @@ function App() {
   return (
     <div className="App">
 
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path = "/" element={<Main/>} />
+        {/*  <Route path = "/" element={<Main/>} />*/}
           <Route path = "/singup" element={<Signup/>} />
           <Route path = "/singup_site" element={ <Signup_site/>} />
           <Route path = "/order/:itemId" element={<Order/>} />
+          <Route path = "/delivery" element={<AddDelivery/>} />
+          <Route path = "/admin" element={<AdminDashboard/>} />
+          <Route path = "/admin/pro" element={<AddProduct/>} />
         </Routes>      
-      </BrowserRouter> */}
+      </BrowserRouter> 
+      {/* <Login/>
+ <Signup_site/>*/}
 
-
-      <Login/>
-      <Signup_site/>
+        <AdminDashboardCopy/>
      
 
     </div>
