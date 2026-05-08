791c9ca5c27528c975b30688cd9efbd4359c9ced
soeun <yuthdms0929@gmail.com>
2026-05-08 14:58:47 +0900

    [feat] 유소은 라우터 등록

--- threeh/src/main/frontend/src/router/Router.js
+++ threeh/src/main/frontend/src/router/Router.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../components/Main';
+import Signup from '../components/Login';
 import Signup from '../components/Signup';
 import Signup_site from '../components/Signup_site';
 import Order from '../components/Order/Order';
@@ -13,6 +14,7 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Main/>} />
+                <Route path="/login" element={<Login/>} />
                 <Route path="/singup" element={<Signup/>} />
                 <Route path="/singup_site" element={<Signup_site/>} />
                 <Route path="/order/:itemId" element={<Order/>} />
