d458110922bcf30f148d38142665b36e9180c375
alphaengneering02-dev <alphaengneering02@gmail.com>
2026-05-08 12:49:02 +0900

    [feat , fix] 라우터 추가 및 폴더 추가
    라우터 폴더 추가
    오더 파일들 오더 폴더 안으로 이동

--- threeh/src/main/frontend/src/App.js
+++ threeh/src/main/frontend/src/App.js
@@ -1,30 +1,12 @@
 import './App.css';
-import{ BrowserRouter, Routes, Route } from 'react-router-dom'
-import Signup from './components/Signup';
-import Signup_site from './components/Signup_site';
-import Order from './components/Order';
-import Main from './components/Main';
-import Login from './components/Login';
-
+import Router from './router/Router';
 
 
 function App() {
   return (
     <div className="App">
 
-      <BrowserRouter>
-        <Routes>
-          <Route path = "/" element={<Main/>} />
-          <Route path = "/singup" element={<Signup/>} />
-          <Route path = "/singup_site" element={ <Signup_site/>} />
-          <Route path = "/order/:itemId" element={<Order/>} />
-        </Routes>      
-      </BrowserRouter>
-
-
-      <Login/>
-      <Signup_site/>
-     
+      <Router />
 
     </div>
   );
