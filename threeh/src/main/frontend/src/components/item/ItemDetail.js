f837b2068f2336414f50a5f97328e41f2466c583
오현옥 <sdolikeim@gmail.com>
2026-05-13 10:51:02 +0900

    [feat]오현옥 상품 페이지 수정

--- threeh/src/main/frontend/src/components/item/ItemDetail.js
+++ threeh/src/main/frontend/src/components/item/ItemDetail.js
@@ -7,33 +7,77 @@ const ItemDetail = () => {
     //URL에서 Item 가져오기
     const {itemId} = useParams();
     
-    //상품 상태 저장
-    const [item,setItem] = useState();
-
-    //컴포넌트 실행시 상세조회 API 호출
+    //상품 상태 관리
+    const [item,setItem] = useState(null);
+    //이미지 상태 관리
+    const [itemImgs,setItemImgs] = useState([]);
 
     useEffect(()=>{
-        axios.get(`/item/${itemId}`)
-            .then((response)=>{
-                console.log(response.data);
-                setItem(response.data);
-            })
-            .catch((error) =>{
-                console.error("상품 상세조회 실패",error);
-            });
+        getItem();
+        getItemImgs();
     },[itemId]);
 
-    //데이터가 없을때
+    //상품정보 동기화
+    const getItem = async() =>{
+        try{
+            const response = await axios.get(
+                `http://localhost:8080/item/${itemId}`
+            );
+            console.log("상품 상세:", response.data);
+            setItem(response.data);
+        }catch(error){
+            console.error("상품 상세조회 실패", error);
+        }
+    };
+
+    //상품이미지 정보 동기화
+
+    const getItemImgs = async() =>{
+        try{
+            const response = await axios.get(
+                `http://localhos:8080/itemImgs/${itemId}`
+            );
+            console.log("상품 이미지: ", response.data);
+            setItemImgs(response.data);
+        }catch(error){
+            console.error("상품 이미지 조회 실패", error);
+        }
+    };
+
     if(!item){
-        return <p>상품 불러오는 중...</p>;
+        return <p>상품 불러오는 중....</p>;
     }
 
+    const mainImg = itemImgs.find((img)=> img.thumbnailYn === "Y");
+    const subImgs = itemImgs.filter((img)=> img.thumbnailYn === "N");
+
+   
     return (
         <div>
             
             <h1>상품 상세 페이지</h1>
 
             <h2>{item.itemName}</h2>
+
+            {mainImg ? (
+                <div>
+                    <h3>대표 이미지</h3>
+                    <img src={`http://localhost:8080${mainImg.itemImgUrl}`} alt={mainImg.itemImgName} width="300"/>
+                </div>
+            ) : (
+                <p>대표 이미지가 없습니다.</p>
+            )}
+
+            {subImgs.length>0 &&(
+                <div>
+                    <h3>서브 이미지</h3>
+
+                    {subImgs.map((img)=>(
+                        <img key={img.itemImgId}src={`http://localhost:8080${img.itemImgUrl}`}alt={img.itemImgName} width="150" style={{marginRight: "10px", marginBottom:"10px",}}/>
+                    ))}
+                </div>
+            )}
+            <hr/>
 
             <p>카테고리: {item.itemCategory}</p>
             <p>상품 설명: {item.ItemDetail}</p>
@@ -42,7 +86,6 @@ const ItemDetail = () => {
             <p>상품 할인가격: {item.itemDiscountPrice}</p>
             <p>상품 최종가격: {item.itemFinalPrice}</p>
             <p>상품 재고: {item.itemStock}</p>
-
         </div>
     );
 };
