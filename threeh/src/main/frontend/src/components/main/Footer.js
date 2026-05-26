e1582edc9a24101ae188f5238e30ee24e00ce846
soeun <yuthdms0929@gmail.com>
2026-05-26 10:10:54 +0900

    [fix]메인-푸터 수정
    클래스명 앞에 main-을 붙임

--- threeh/src/main/frontend/src/components/main/Footer.js
+++ threeh/src/main/frontend/src/components/main/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <div className="footer-content">
-                <p className="footer-logo">PROJECT CMYK</p>
-                <p className="footer-info">주식회사 씨엠와이케이 | 공동 프로젝트 팀 | 경기도 수원시 팔달구</p>
-                <p className="footer-copy">© 2026 PROJECT CMYK. All Rights Reserved.</p>
+            <div className="main-footer-content">
+                <p className="main-footer-logo">PROJECT CMYK</p>
+                <p className="main-footer-info">주식회사 씨엠와이케이 | 공동 프로젝트 팀 | 경기도 수원시 팔달구</p>
+                <p className="main-footer-copy">© 2026 PROJECT CMYK. All Rights Reserved.</p>
             </div>
         </footer>
     );
