08571635fc535f5f43e65565a3b306562b5fca13
alphaengneering02-dev <alphaengneering02@gmail.com>
2026-05-26 17:27:44 +0900

    [refactory] 메인페이지
    메인 페이지 이미지 리팩토링

--- threeh/src/main/frontend/src/components/main/Main_itemList_item.js
+++ threeh/src/main/frontend/src/components/main/Main_itemList_item.js
@@ -7,25 +7,6 @@ const Main_itemList_item = ({item, getLoginUser, isUserRole, isUser}) => {
     //이미지 상태 객체
     const [itemImgs, setItemImgs] = useState({});
     
-
-    //DB에서 각 상품의 이미지를 가져오는 함수
-    const getItemImg = async() =>{
-        try{
-            //상품 이미지 가져오기
-            const res = await axios.get(
-                `http://localhost:8080/api/${item.itemId}`
-            );
-            setItemImgs(res.data)
-
-            //콘솔 출력
-            console.log(`[${item.itemName} 상품의 이미지 가져오기 완료]\n`, res.data)
-
-        } catch(error){
-            console.error(`[${item.itemName} 상품의 이미지 가져오기 실패]\n`, error);
-        }
-    };
-
-
     //상품 최종 가격 계산
     //백엔드에서 itemFinalPrice가 오면 그 값을 사용
     //없으면 원가-할인금액으로 계산
@@ -161,7 +142,7 @@ const Main_itemList_item = ({item, getLoginUser, isUserRole, isUser}) => {
         <div className="main-item-card">
             <div className="main-image-box">
                 {/* 상품 이미지 */}
-                <img src={item.imgUrl || "placeholder.png"} alt={item.itemName} />
+                <img src={item.itemImgUrl} alt={item.itemName} />
                 {/* 북마크 버튼 */}
                 {isUser && (
                     <button
