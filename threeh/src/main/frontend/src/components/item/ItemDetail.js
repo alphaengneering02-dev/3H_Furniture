e99043e4615ec58babe224c04984d489b0881952
오현옥 <sdolikeim@gmail.com>
2026-05-29 14:47:57 +0900

    [feat]상품상세페이지 카운트 수정

--- threeh/src/main/frontend/src/components/item/ItemDetail.js
+++ threeh/src/main/frontend/src/components/item/ItemDetail.js
@@ -460,7 +460,7 @@ const ItemDetail = () => {
    const getItemOrderCount = async () => {
     try {
         const response = await axios.get(
-            "http://localhost:8080/api/orders",
+            "http://localhost:8080/admin/orders",
             {
                 withCredentials: true,
             }
