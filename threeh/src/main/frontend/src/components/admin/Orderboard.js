91dcb2f88e05aff0ad10822d58b417ac1b80510b
taeyang999-kim <sunsun9579@naver.com>
2026-05-29 15:36:51 +0900

    [feat]김태양 css 수정중

--- threeh/src/main/frontend/src/components/admin/Orderboard.js
+++ threeh/src/main/frontend/src/components/admin/Orderboard.js
@@ -229,7 +229,7 @@ const Orderboard = ({
                 <h3>배송 배정 완료 목록[O.S=READY,D.S=WAITING인 ordersDB]</h3>
                 </div>
                 <table className="admin-table-style">
-                    <thead><tr><th>번호</th><th>상품</th><th>판매 금액</th><th>배정된 기사</th><th>상태</th><th>주문일</th></tr></thead>
+                    <thead><tr><th>번호</th><th>상품</th><th>배송지</th><th>배정된 기사</th><th>상태</th><th>주문일</th></tr></thead>
                     <tbody>
                         {pagedAssigned.map((order, index) => (
                             <tr key={order.orderId}>
